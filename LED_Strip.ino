2e06a7d4c7ac0ec28c8800f2bfbfdb931c45275e
Jannis Mühlemeyer <jannis.muehlemeyer@outlook.com>
2017-12-13 22:32:26 +0100

    LED Ansteuerung geht + WLAN Config

--- LED_Strip.ino
+++ LED_Strip.ino
@@ -1,131 +1,308 @@
-#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
-#include "FastLED.h"
-
-FASTLED_USING_NAMESPACE
-
 //Doku 1: https://www.xgadget.de/anleitung/arduino-ws2812b-leds-mit-fastled-ansteuern/
 //Doku Lib: https://github.com/FastLED/FastLED
 //ESP8266 Notes: https://github.com/FastLED/FastLED/wiki/ESP8266-notes
 
-// FastLED "100-lines-of-code" demo reel, showing just a few 
-// of the kinds of animation patterns you can quickly and easily 
-// compose using FastLED.  
-//
-// This example also shows one easy way to define multiple 
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
+//#define FASTLED_ESP8266_NODEMCU_PIN_ORDER -> dann geht 3 statt D3
+#define FASTLED_ESP8266_RAW_PIN_ORDER //-> dann geht D3 statt 3
+//#define FASTLED_ALLOW_INTERRUPTS 0
+#define FASTLED_INTERRUPT_RETRY_COUNT 1
+#include "FastLED.h"
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
 
-#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
-#warning "Requires FastLED 3.1 or later; check github for latest code."
-#endif
+//Einstellungen
+#define LED_TYPE WS2812B
+#define RGB_ORDER GRB
+const byte ANZAHL_LEDS = 120;
+const byte PIN_LED_DATA = D3;
+const byte LED_DEFAULT_HELLIGKEIT = 15;
+//ENDE Einstellungen
 
-#define DATA_PIN    3
-//#define CLK_PIN   4
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-#define NUM_LEDS    120
-CRGB leds[NUM_LEDS];
+//## WiFi
+const String newLine = "\n";
+const byte connectionTimeoutInSekunden = 20;
+MDNSResponder mdns;
+ESP8266WebServer server(80);
+//## ENDE WiFi
 
-#define BRIGHTNESS          60
-#define FRAMES_PER_SECOND  120
+//## EEPROM Adressen
+const byte ssidLaengeAdresse = 0;
+const byte passwordLaengeAdresse = 1;
+const byte ssidStartAdresse = passwordLaengeAdresse + 1;
+byte passwortStartAdresse;
+const byte eepromUsedBytes = 195;
+//##ENDE EEPROM Adressen
+
+CRGB leds[ANZAHL_LEDS];
 
 void setup() {
-  delay(3000); // 3 second delay for recovery
-  
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, PIN_LED_DATA, RGB_ORDER>(leds, ANZAHL_LEDS);
+  FastLED.setBrightness(LED_DEFAULT_HELLIGKEIT);
+  for (int i = 0; i < ANZAHL_LEDS; i++) {
+    leds[i].setRGB(0, 0, 0);
+  }
+  FastLED.show();
 
-  // set master brightness control
-  FastLED.setBrightness(BRIGHTNESS);
+  //WLAN Zugangsdaten aus EEPROM lesen
+  byte ssidLaenge = LeseByteAusEeprom(ssidLaengeAdresse);
+  byte passwordLaenge = LeseByteAusEeprom(passwordLaengeAdresse);
+  byte passwortStartAdresse = ssidStartAdresse + ssidLaenge + 1;
+
+  char ssid[ssidLaenge + 1];
+  LeseStringAusEeprom(ssidStartAdresse, ssidLaenge, ssid);
+
+  char passwort[passwordLaenge + 1];
+  LeseStringAusEeprom(passwortStartAdresse, passwordLaenge, passwort);
+  //ENDE WLAN Zugangsdaten aus EEPROM lesen
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, passwort);
+  unsigned long timeout = millis()  + (connectionTimeoutInSekunden * 1000);
+  bool wifiConfigMode = false;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    if (millis() > timeout) {
+      //Wir konnten uns nicht mit dem angegebenen WLAN verbinden.
+      wifiConfigMode = true;
+      break;
+    }
+  }
+  if (wifiConfigMode) {
+    //Eigenes WLAN aufspannen
+    WiFi.mode(WIFI_AP);
+    IPAddress ip(192, 168, 0, 1);
+    IPAddress mask(255, 255, 255, 0);
+    WiFi.softAPConfig(ip, ip, mask);
+    WiFi.softAP("LED RGB", "setupLedStrip2017");
+  }
+
+  mdns.begin("esp8266", WiFi.localIP());
+
+  if (wifiConfigMode) {
+    server.on("/", handleWlanKonfiguration);
+  } else {
+    server.on("/wlan", handleWlanKonfiguration);
+  }
+
+  server.begin();
 }
 
-
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
-
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-  
-void loop()
-{
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
-
-  // send the 'leds' array out to the actual LED strip
-  FastLED.show();  
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
-
-  // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+void loop() {
+  server.handleClient();
+  yield();
+  //RunTestmode();
 }
 
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
-void nextPattern()
-{
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+void RunTestmode() {
+  RunTestmodeWithColor(255, 0, 0);
+  RunTestmodeWithColor(0, 255, 0);
+  RunTestmodeWithColor(0, 0, 255);
 }
 
-void rainbow() 
-{
-  // FastLED's built-in rainbow generator
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
-}
-
-void rainbowWithGlitter() 
-{
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(80);
-}
-
-void addGlitter( fract8 chanceOfGlitter) 
-{
-  if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
+void RunTestmodeWithColor(byte r, byte g, byte b) {
+  for (byte i = 0; i < ANZAHL_LEDS; i++) {
+    if (i == 0) {
+      leds[ANZAHL_LEDS - 1].setRGB(0, 0, 0);
+    } else {
+      leds[i - 1].setRGB(0, 0, 0);
+    }
+    leds[i].setRGB(r, g, b);
+    FastLED.show();
+    delay(300);
   }
 }
 
-void confetti() 
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+void handleWlanKonfiguration() {
+  if (server.args() == 2) {
+    String ssid = server.arg("ssid");
+    String passwort = server.arg("passwort");
+    ssid.replace("+", " ");
+    passwort.replace("+", " ");
+    if (ssid.length() < 1 || passwort.length() < 1) {
+      server.send(501, "text/plain", "SSID oder Passwort Laenge war 0!");
+      return;
+    }
+    //SSID in EEPROM speichern
+    byte laengeSsid = ssid.length();
+    byte langePasswort = passwort.length();
+    if (laengeSsid > 128 || langePasswort > 128) {
+      server.send(501, "text/plain", "SSID oder Passwort war laenger als 128 Zeichen!");
+      return;
+    }
+    SpechereByteInEeprom(laengeSsid, ssidLaengeAdresse);
+    SpechereByteInEeprom(langePasswort, passwordLaengeAdresse);
+    SpeichereStringInEeprom(ssid, ssidStartAdresse);
+    byte passwortStartAdresse = ssidStartAdresse + laengeSsid + 1;
+    SpeichereStringInEeprom(passwort, passwortStartAdresse);
+    //Lesen zum 1zu1 ausgeben:
+    byte ssidLaengeR = LeseByteAusEeprom(ssidLaengeAdresse);
+    byte passwordLaengeR = LeseByteAusEeprom(passwordLaengeAdresse);
+    byte passwortStartAdresseR = ssidStartAdresse + ssidLaengeR + 1;
+
+    char ssidR[ssidLaengeR + 1];
+    LeseStringAusEeprom(ssidStartAdresse, ssidLaengeR, ssidR);
+
+    char passwortR[passwordLaengeR + 1];
+    LeseStringAusEeprom(passwortStartAdresseR, passwordLaengeR, passwortR);
+
+    //WLAN gespeichert
+    String html = F("<!DOCTYPE html>");
+    html += newLine;
+    html += F("<html lang=\"de\">");
+    html += newLine;
+    html += F("<head>");
+    html += newLine;
+    html += F("<meta charset=\"UTF-8\">");
+    html += newLine;
+    html += F("<link rel=\"stylesheet\" href=\"http://yui.yahooapis.com/pure/0.6.0/pure-min.css\">");
+    html += newLine;
+    html += F("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+    html += newLine;
+    html += F("<title>WLAN gespeichert</title>");
+    html += newLine;
+    html += F("</head>");
+    html += newLine;
+    html += F("<body>");
+    html += newLine;
+    html += F("<div class=\"pure-g\">");
+    html += newLine;
+    html += F("<div class=\"pure-u-1 pure-u-md-1-1\">");
+    html += newLine;
+    html += F("<h1>WLAN gespeichert</h1>");
+    html += newLine;
+    html += F("<p>SSID und Passwort wurden gespeichert.</p>");
+    html += newLine;
+    html += F("<p><b>Entfernen Sie für circa 5 Sekunden den Stromstecker und verbinden Sie ihn danach wieder, damit der Microcontroller neu startet!</b></p>");
+    html += newLine;
+    html += F("<p>Er wird sich danach mit dem angegebenen WLAN verbinden:</p>");
+    html += newLine;
+    html += "<p>SSID: '" + String(ssidR) + "'</p>";
+    html += newLine;
+    html += "<p>Passwort: '" + String(passwortR) + "'</p>";
+    html += newLine;
+    html += F("<p>Jeweils ohne die '-Zeichen zu Beginn / Ende)</p>");
+    html += newLine;
+    html += F("</div>");
+    html += newLine;
+    html += F("</div>");
+    html += newLine;
+    html += F("</body>");
+    html += newLine;
+    html += F("</html>");
+    html += newLine;
+
+    server.send(200, "text/html", html);
+    delay(5000); //server.send bearbeiten
+    while (true) {
+      delayMicroseconds(1000); //Keine HTTP Anfragen mehr bearbeiten. IC soll ja neu gestartet werden
+    }
+  }
+  //WLAN Daten eingeben
+  String html = F("<!DOCTYPE html>");
+  html += newLine;
+  html += F("<html lang=\"de\">");
+  html += newLine;
+  html += F("<head>");
+  html += newLine;
+  html += F("<meta charset=\"UTF-8\">");
+  html += newLine;
+  html += F("<title>WLAN-Konfiguration</title>");
+  html += newLine;
+  html += F("<link rel=\"stylesheet\" href=\"http://yui.yahooapis.com/pure/0.6.0/pure-min.css\">");
+  html += newLine;
+  html += F("<link rel=\"stylesheet\" href=\"http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css\">");
+  html += newLine;
+  html += F("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+  html += newLine;
+  html += F("</head>");
+  html += newLine;
+  html += F("<body>");
+  html += newLine;
+  html += F("<div class=\"pure-g\">");
+  html += newLine;
+  html += F("<div class=\"pure-u-1 pure-u-md-1-1\">");
+  html += newLine;
+  html += F("<h1>LED Strip</h1>");
+  html += newLine;
+  html += F("<p>Die SSID und Passwort dürfen jeweils eine Länge von 128 Zeichen nicht überschreiten.</p>");
+  html += newLine;
+  html += "<p>" + GetWlanNetzwerke() + "</p>";
+  html += newLine;
+  html += F("<fieldset>");
+  html += newLine;
+  html += F("<legend>WLAN Zugangsdaten eingeben</legend>");
+  html += newLine;
+  html += F("<form class=\"pure-form\">");
+  html += newLine;
+  html += F("<input type=\"text\" name=\"ssid\" required autofocus placeholder=\"SSID\">");
+  html += newLine;
+  html += F("<input type=\"password\" name=\"passwort\" required autofocus placeholder=\"Passwort\">");
+  html += newLine;
+  html += F("<br>");
+  html += newLine;
+  html += F("<button type=\"submit\" class=\"pure-button ion-checkmark-round\"> Bestätigen</button>");
+  html += newLine;
+  html += F("</form>");
+  html += newLine;
+  html += F("</fieldset>");
+  html += newLine;
+  html += F("</div>");
+  html += newLine;
+  html += F("</div>");
+  html += newLine;
+  html += F("</body>");
+  html += newLine;
+  html += F("</html>");
+  html += newLine;
+
+
+  server.send(200, "text/html", html);
 }
 
-void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
-  leds[pos] += CHSV( gHue, 255, 192);
+String GetWlanNetzwerke() {  
+  int numSsid = WiFi.scanNetworks();
+  if (numSsid == -1) {
+    return "<b>Keine Netzwerke gefunden</b><br>";
+  }
+  String n = "<b>" + String(numSsid) + " Netzwerke gefunden </b><br>";
+  for (int thisNet = 0; thisNet < numSsid; thisNet++) {
+    n += String(WiFi.SSID(thisNet)) + "<br>";
+  }
+  return n;
 }
 
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_LEDS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+bool SpeichereStringInEeprom(String value, int startAdresse) {
+  EEPROM.begin(eepromUsedBytes);
+  int curAdresse = startAdresse;
+  for (int i = 0; i < value.length(); i++) {
+    char curChar = value[i];
+    EEPROM.write(curAdresse, curChar);
+    curAdresse++;
+  }
+  bool success =  EEPROM.commit();
+  EEPROM.end();
+  return success;
+}
+
+void LeseStringAusEeprom(int startAdresse, int laenge, char *outResultChar) { //outResultChar Länge = laenge + 1 !!!!
+  EEPROM.begin(eepromUsedBytes);
+  outResultChar[laenge] = '\0'; //Nullterminierung: Wenn laenge 4 ist, geht Array Index von 0-4 (laenge + 1). Dabei ist Index 0-3 EEPROM, Index 4 null-Terminierung
+  for (int i = 0; i < laenge; i++) {
+    outResultChar[i] = EEPROM.read(startAdresse + i);
   }
 }
 
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
+bool SpechereByteInEeprom(byte wert, int adresse) {
+  EEPROM.begin(eepromUsedBytes);
+  EEPROM.write(adresse, wert);
+  bool success =  EEPROM.commit();
+  EEPROM.end();
+  return success;
 }
 
+byte LeseByteAusEeprom(int adresse) {
+  EEPROM.begin(eepromUsedBytes);
+  return EEPROM.read(adresse);
+}
